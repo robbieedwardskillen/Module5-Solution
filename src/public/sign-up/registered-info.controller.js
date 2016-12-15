35aca6ca12a180d2ddb3702e68c670542f1064fa
robbieskillen <rskill01@baker.edu>
2016-12-15 01:33:02 -0500

    commit #13

--- src/public/sign-up/registered-info.controller.js
+++ src/public/sign-up/registered-info.controller.js
@@ -10,9 +10,11 @@ function RegisteredInfoController(SubmissionService, MenuService, ApiPath, allMe
 	$ctrl.basePath = ApiPath;
 	$ctrl.info = SubmissionService.getInformation();
 	$ctrl.valid = false;
+	$ctrl.favDish = "";
 	for(var i = 0; i < allMenuItems.menu_items.length; i++){
 		if (allMenuItems.menu_items[i].short_name == $ctrl.info.dish){
 			$ctrl.valid = true;
+			$ctrl.favDish = allMenuItems.menu_items[i].short_name;
 		}
 	}
 }
